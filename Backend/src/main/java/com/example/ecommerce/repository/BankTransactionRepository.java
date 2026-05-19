74495cf1c4c99ae30ce266b8fd0df9d05cb94a02
Manh12347 <lemanh151148@gmail.com>
2026-05-19 08:51:14 +0700

    Setup Codebase Web

--- Backend/src/main/java/com/example/ecommerce/repository/BankTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entity.BankTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BankTransactionRepository extends JpaRepository<BankTransaction, Long> {
-}
