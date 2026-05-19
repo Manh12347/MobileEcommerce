39adc87bcbdeee0d4a62ce713f7f44acff7fc407
Manh12347 <lemanh151148@gmail.com>
2026-05-19 08:51:14 +0700

    Setup Codebase Web

--- Backend/src/main/java/com/example/ecommerce/repository/SoldSerialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entity.SoldSerial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SoldSerialRepository extends JpaRepository<SoldSerial, Integer> {
-}
