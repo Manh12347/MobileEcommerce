31a4a322098023c11398941208a43d5e1ba0efbb
Manh12347 <lemanh151148@gmail.com>
2026-05-23 21:39:04 +0700

    Clean main-web code

--- Backend/src/main/java/com/example/ecommerce/repository/WarrantyClaimRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entity.WarrantyClaim;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WarrantyClaimRepository extends JpaRepository<WarrantyClaim, Integer> {
-}
